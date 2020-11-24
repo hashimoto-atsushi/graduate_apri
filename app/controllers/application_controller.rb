c636efb21370d53d4dfbeec7e6407b1cca2def7a
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-11-24 19:32:59 +0900

    Add User controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :name, :department, :position, :permission, :cellphone_number, :stay_or_left])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:employee_number, :name, :department, :position, :permission, :cellphone_number, :stay_or_left])
   end
 end
