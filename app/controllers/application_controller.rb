cc6460235cdc3e8ffbe020b47f1e0e0f4df6c263
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-12-02 20:05:02 +0900

    deviseアクセス制限 OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,18 +34,9 @@ class ApplicationController < ActionController::Base
     current_user.permission || current_user.department == "技術部"
   end
 
-  def tech_in_charge_and_admin_login
-    current_user == @support_program.user || current_user.permission
-  end
-
   def boss_and_admin_display_login
     boss_status = current_user.position == "部長" && current_user.department == "技術部"
     boss_status || current_user.permission
   end
-
-  # def sales_in_charge_and_admin_login
-  # end
-
-
 
 end
