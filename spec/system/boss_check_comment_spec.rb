c8c60e8ca31edb30532733afbc881adc1fa341b3
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-12-09 15:08:16 +0900

    Rspec修正

--- spec/system/boss_check_comment_spec.rb
+++ spec/system/boss_check_comment_spec.rb
@@ -23,9 +23,12 @@ RSpec.describe '上長確認 登録管理機能', type: :system do
         click_on '上長確認 登録へ'
         select '定期保守A', from: 'boss_check_comment[detail_report_id]'
         select '承認済', from: 'boss_check_comment[confirmation]'
-        select '2021', from: 'boss_check_comment[confirmation_date(1i)]'
-        select '11月', from: 'boss_check_comment[confirmation_date(2i)]'
-        select '26', from: 'boss_check_comment[confirmation_date(3i)]'
+        fill_in 'boss_check_comment_confirmation_date', with: '002021-11-26'
+
+
+        # select '2021', from: 'boss_check_comment[confirmation_date(1i)]'
+        # select '11月', from: 'boss_check_comment[confirmation_date(2i)]'
+        # select '26', from: 'boss_check_comment[confirmation_date(3i)]'
         fill_in 'boss_check_comment_positive_comment', with: '内容了解しました。安全に作業していただきありがとうございます。'
         fill_in 'boss_check_comment_negative_comment', with: '特にはありませんが、できるだけ早めに報告を上げていだけると幸いです。'
         fill_in 'boss_check_comment_assistance', with: 'この調子でよろしくお願いします。営業部には私から連絡しておきます。'
@@ -39,6 +42,7 @@ RSpec.describe '上長確認 登録管理機能', type: :system do
         expect(ActionMailer::Base.deliveries.second.to).to eq ['boss@example.com']
         visit top_index_path
         visit boss_check_comments_path
+        click_on '詳細'
         expect(page).to have_content '内容了解しました。安全に作業していただきありがとうございます。'
         expect(page).to have_content '特にはありませんが、できるだけ早めに報告を上げていだけると幸いです。'
         expect(page).to have_content 'この調子でよろしくお願いします。営業部には私から連絡しておきます。'
@@ -52,6 +56,7 @@ end
 private
 def log_in
   visit new_user_session_path
+  fill_in 'user_employee_number', with: 4
   fill_in 'user_email', with: 'boss@example.com'
   fill_in 'user_password', with: 'testtest'
   click_on 'ログイン'
