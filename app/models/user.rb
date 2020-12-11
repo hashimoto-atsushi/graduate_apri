4b2cc4b6b1ff5ead40dd20db3a47cabd4b7aa6c3
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-12-11 22:06:36 +0900

    uniqueのヴァリデーションテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  validates :employee_number, presence: true
+  validates :employee_number, presence: true,
+                            uniqueness: true
   validates :name, presence: true,
                      length: { maximum: 32 }
   validates :password, presence: true,
                          length: { minimum: 6 }
   validates :email, presence: true,
+                    uniqueness: true,
                       length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
--- db/schema.rb
+++ db/schema.rb
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_060910) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["employee_number"], name: "index_users_on_employee_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
