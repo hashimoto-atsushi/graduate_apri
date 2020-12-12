966be6c3e6d7d7e43ea1de4f81503ec7ca6ee56e
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-12-12 18:34:17 +0900

    save

--- app/controllers/allsupports_controller.rb
+++ app/controllers/allsupports_controller.rb
@@ -1,6 +1,6 @@
 class AllsupportsController < ApplicationController
   before_action :authenticate_user!
-  PER = 20
+  PER = 35
   def index
     @support_programs = SupportProgram.all.order(title_number: :asc).page(params[:page]).per(PER)
   end
