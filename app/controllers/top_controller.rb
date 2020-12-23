c3271d95903b70227266ca672f3b3cc8c939786e
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-12-24 07:24:42 +0900

    ゲストログイン実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,12 @@
 class TopController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:new_guest]
 
   def index
+  end
+
+  def new_guest
+    user = User.find_by(email: 'tech1@example.com')
+    sign_in user
+    redirect_to top_index_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
